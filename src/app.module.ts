a2ac48f2f8ff57f638ecbae570f443ec35b33ce3
Rafael <rflernesto@gmail.com>
2023-02-23 22:33:09 -0400

    chore: created image-list module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageModule } from './image/image.module';
+import { ImageListModule } from './image-list/image-list.module';
 
 @Module({
-  imports: [ImageModule],
+  imports: [ImageModule, ImageListModule],
   controllers: [AppController],
   providers: [AppService],
 })
