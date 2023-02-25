1cca254a977edc1d307e47b1ea076571da4963a3
Rafael <rflernesto@gmail.com>
2023-02-25 14:35:30 -0400

    chore: register providers to image-list module

--- src/image-list/image-list.module.ts
+++ src/image-list/image-list.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ImageListController } from './controllers/image-list.controller';
+import { DynamoDbImageListRepository } from './repositories/DynamoDbImageListRepository.repository';
+import { ImageListRepository } from './repositories/image-list.repository';
 import { ImageListService } from './services/image-list-service';
 
 @Module({
   controllers: [ImageListController],
-  providers: [ImageListService],
+  providers: [
+    { provide: ImageListService, useClass: ImageListService },
+    { provide: ImageListRepository, useClass: DynamoDbImageListRepository },
+  ],
 })
 export class ImageListModule {}
--- src/image/repositories/DynamoDbImageRepository.service.ts
+++ src/image/repositories/DynamoDbImageRepository.service.ts
@@ -2,6 +2,18 @@ import { Image } from '../entities/image.entity';
 import { ImageRepositoryService } from './ImageRepository.service';
 
 export class DynamoDbImageRepository extends ImageRepositoryService {
+  get(imageId: string): Promise<Image> {
+    throw new Error('Method not implemented.');
+  }
+  getAll(userId: string): Promise<Image[]> {
+    throw new Error('Method not implemented.');
+  }
+  update(updatedData: Image): Promise<Image> {
+    throw new Error('Method not implemented.');
+  }
+  delete(imageId: string): Promise<null> {
+    throw new Error('Method not implemented.');
+  }
   save(image: Image): Promise<null> {
     throw new Error('Method not implemented.');
   }
