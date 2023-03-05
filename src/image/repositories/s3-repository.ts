396fe57060606c0061c3acbe99895fc2e6f6fe0b
Rafael <rflernesto@gmail.com>
2023-03-05 15:40:09 -0400

    feat: implement file upload

--- src/image/repositories/s3-repository.ts
+++ src/image/repositories/s3-repository.ts
@@ -1,7 +1,21 @@
 import { ImageStorageRepositoryService } from './image-storage.repository';
+import { S3 } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+import { ConfigService } from '@nestjs/config';
 
 export class S3Repository extends ImageStorageRepositoryService {
-  save(image: Express.Multer.File): Promise<string> {
-    throw new Error('Method not implemented.');
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+  async save(image: Express.Multer.File): Promise<string> {
+    const s3 = new S3();
+    const uploadResult = await s3
+      .upload({
+        Bucket: this.configService.get('AWS_BUCKET_NAME'),
+        Body: image,
+        Key: `${uuid()}-${image.filename}`,
+      })
+      .promise();
+    return uploadResult.Location;
   }
 }
