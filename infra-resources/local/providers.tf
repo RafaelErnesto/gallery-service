63cd2c6fe1cf6e304d784c05e364680b653b06dd
Rafael <rflernesto@gmail.com>
2023-03-06 21:20:11 -0400

    ci: add apigateway endpoint for localstack

--- infra-resources/local/providers.tf
+++ infra-resources/local/providers.tf
@@ -12,5 +12,6 @@ provider "aws" {
     lambda = "http://localhost:4566"
     logs   = "http://localhost:4566"
     iam    = "http://localhost:4566"
+    apigateway  = "http://localhost:4566"
   }
 }
