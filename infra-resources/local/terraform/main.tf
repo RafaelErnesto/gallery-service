a94eb1ce8a7f9a58de447235a96e58803c1fdb74
Rafael <rflernesto@gmail.com>
2023-04-07 22:01:36 -0400

    chore: add api gateway deployment for api gateway

--- infra-resources/local/terraform/main.tf
+++ infra-resources/local/terraform/main.tf
@@ -20,8 +20,18 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.gallery-service-rest.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
-
+  integration_http_method = "ANY"
   uri = aws_lambda_function.gallery-service.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api-gateway-deployment" {
+  rest_api_id = aws_api_gateway_rest_api.gallery-service-rest.id
+}
+
+resource "aws_api_gateway_stage" "api-gateway-stage" {
+  deployment_id = aws_api_gateway_deployment.api-gateway-deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.gallery-service-rest.id
+  stage_name    = "local-gallery"
 }
 
 resource "aws_iam_role" "fake_lambda_role" {
@@ -50,7 +60,7 @@ resource "aws_s3_bucket" "dev-images" {
 
 resource "aws_lambda_function" "gallery-service" {
   function_name = "gallery-service"
-  filename      = "function.zip"
+  filename      = "../../../dist/function.zip"
   handler       = "main"
   role          = aws_iam_role.fake_lambda_role.arn
   runtime       = "nodejs16.x"
