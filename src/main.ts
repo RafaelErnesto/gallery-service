249fb0486bb83d5c9cfaa68347800d52e32e76e3
Rafael <rflernesto@gmail.com>
2023-04-09 15:43:43 -0400

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
       skipNullProperties: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Gallery Service')
+    .setDescription('The gallery-service API description')
+    .setVersion('1.0')
+    .addTag('galllery-service')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.init();
 }
