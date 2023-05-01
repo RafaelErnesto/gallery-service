dc56b479d6e40fb82fa00efcec9b3477ea526cab
Rafael Ernesto <rflernesto@gmail.com>
2023-05-01 18:29:01 -0400

    chore: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ImageModule } from './image/image.module';
 import { ImageListModule } from './image-list/image-list.module';
 import { getMongooseModule } from './utils/mongodb.utils';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ImageModule, ImageListModule, getMongooseModule()],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration]
+  }), ImageModule, ImageListModule, getMongooseModule()],
 })
-export class AppModule {}
+export class AppModule { }
